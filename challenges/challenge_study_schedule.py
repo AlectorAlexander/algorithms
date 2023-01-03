e301d1d49c0a11c9fb507a26e2df77a451463fe4
Aléctor Alexander <alector.alexandre@gmail.com>
2023-01-03 16:19:52 -0300

    req01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,20 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not confer_values(permanence_period, target_time):
+        return None
+    count = 0
+    for first_period, second_period in permanence_period:
+        if first_period is not None and second_period is not None:
+            if first_period <= target_time and second_period >= target_time:
+                count += 1
+    return count
+
+
+def confer_values(permanence_period, target_time):
+    if not isinstance(target_time, int) or not all(
+        isinstance(item, tuple) for item in permanence_period
+    ):
+        return False
+    for first_period, second_period in permanence_period:
+        if not all(isinstance(x, int) for x in (first_period, second_period)):
+            return False
+    return True
