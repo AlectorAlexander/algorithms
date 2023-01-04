7740776e7b02b6eb460fed15c126e3c17b9c836f
Aléctor Alexander <alector.alexandre@gmail.com>
2023-01-04 16:14:27 -0300

    re05

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,23 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    counts = create_count(nums)
+
+    if not counts:
+        return False
+
+    for num, count in counts.items():
+        if count > 1:
+            return num
+
+    return False
+
+
+def create_count(nums):
+    counts = {}
+    for num in nums:
+        if isinstance(num, str) or num < 0:
+            return False
+        elif num not in counts:
+            counts[num] = 1
+        else:
+            counts[num] += 1
+    return counts
